d36c41fb2a9ded77edd998930b6969bbb91aad80
Rezo <gomojaradu@gmail.com>
2019-01-23 22:49:42 +0200

    init

---
+++ Js/index.js
@@ -0,0 +1,6 @@
+var nume=prompt("cum te numesti?");
+var fam=prompt("cum familia?");
+var vir=prompt("citi ani ai ?");
+console.log("numele tau deplin este " + nume + " " + fam);
+console.log("virsta ta este " + vir);
+alert("tu ai trait " + vir*365 + " zile");
---
+++ bootstrap/script.js
@@ -0,0 +1,4 @@
+
+var nume=prompt("cum te cheama?");
+alert(nume + ", idi nahui dara");
+console.log(nume + ":sam pashol nahui ");
